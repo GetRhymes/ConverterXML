9bb6de115276945c94a3ded6b0f8ca62d7583c86
Тарасенко Никита Сергеевич <NSTarasenko@sberbank.ru>
2021-11-03 03:30:37 +0300

    Add endpoint in controller for convert Xml to Yaml Add method in service for convert Xml to Yaml Add unit test

--- src/main/kotlin/com/poly/converter/ConvertService.kt
+++ src/main/kotlin/com/poly/converter/ConvertService.kt
@@ -1,5 +1,9 @@
 package com.poly.converter
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+
 import org.json.XML
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -9,5 +13,14 @@ class ConvertService {
     fun convertXMLToJSON(xmlFile: MultipartFile): String {
         val xml = String(xmlFile.bytes)
         return XML.toJSONObject(xml).toString()
+    }
+
+    fun convertXMLToYAML(xml: MultipartFile): String {
+        return convertJSONToYAML(convertXMLToJSON(xml))
+    }
+
+    private fun convertJSONToYAML(json: String): String {
+        val jsonNodeTree: JsonNode = ObjectMapper().readTree(json)
+        return YAMLMapper().writeValueAsString(jsonNodeTree)
     }
 }
